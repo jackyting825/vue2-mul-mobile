ebdde50e5224023a1cda5d0865b23338b8a1c97c
jackyting825@gmail.com <jackying825@gmail.com>
2018-07-13 11:47:31 +0800

    工具类增加等比缩放图片的处理方法

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -47,3 +47,39 @@ export function base64ToImageFile(urlData) {
   // let file = new File([blob], 'blob.png', { type: 'image/png' })
   return blob
 }
+
+/**
+*等比缩放图片到某个尺寸
+@param {图片,必选} img
+@param {要缩放到的最大宽度,可选.默认是浏览器窗口文档显示区的宽度} maxWidth
+@param {要缩放到的最大高度,可选.默认是浏览器窗口文档显示区的高度} maxHeight
+*/
+export function scaleImage(img, maxWidth, maxHeight) {
+  // 等比缩放图片,参数(图片,允许的宽度,允许的高度)
+  if (!maxWidth) {
+    maxWidth = document.documentElement.clientWidth
+  }
+  if (!maxHeight) {
+    maxHeight = document.documentElement.clientHeight
+  }
+  let image = new Image()
+  image.src = img.src
+  image.width = img.naturalWidth
+  image.height = img.naturalHeight
+  if (image.width < maxWidth && image.height < maxHeight) {
+    img.width = image.width
+    img.height = image.height
+  } else {
+    // 原图片宽高比例 大于 图片框宽高比例,则以框的宽为标准缩放，反之以框的高为标准缩放
+    if (maxWidth / maxHeight <= image.width / image.height) {
+      // 原图片宽高比例 大于 图片框宽高比例
+      img.width = maxWidth // 以框的宽度为标准
+      img.height = maxWidth * (image.height / image.width)
+    } else {
+      // 原图片宽高比例 小于 图片框宽高比例
+      img.width = maxHeight * (image.width / image.height)
+      img.height = maxHeight // 以框的高度为标准
+    }
+  }
+  return img
+}
